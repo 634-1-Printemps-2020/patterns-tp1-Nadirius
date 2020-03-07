f942357ad47c1bdb814350134db57ac7916c1854
Nadirius <nadir.zemrani@etu.hesge.ch>
2020-03-07 22:50:26 +0100

    rendu

--- src/metier/PyRat.java
+++ src/metier/PyRat.java
@@ -3,10 +3,24 @@ package metier;
 import java.util.*;
 
 public class PyRat {
+    private List<Point> fromageList;
+    private Map<Point, List<Point>> laby;
+    private Set<Point> fromageSet;
+    private Map<Point, Set<Point>> passageOrdreConstant;
+
 
     /* Méthode appelée une seule fois permettant d'effectuer des traitements "lourds" afin d'augmenter la performace de la méthode turn. */
     public void preprocessing(Map<Point, List<Point>> laby, int labyWidth, int labyHeight, Point position, List<Point> fromages) {
+        this.fromageSet = new HashSet<>(fromages);
+        this.fromageList = fromages;
+        this.laby = laby;
+        this.passageOrdreConstant = new HashMap<>();
+        for (Map.Entry<Point, List<Point>> items : laby.entrySet()) {
+            passageOrdreConstant.put(items.getKey(), new HashSet<>(items.getValue()));
+        }
     }
+
+
 
     /* Méthode de test appelant les différentes fonctionnalités à développer.
         @param laby - Map<Point, List<Point>> contenant tout le labyrinthe, c'est-à-dire la liste des Points, et les Points en relation (passages existants)
@@ -14,8 +28,8 @@ public class PyRat {
         @param position - Point contenant la position actuelle du joueur
         @param fromages - List<Point> contenant la liste de tous les Points contenant un fromage. */
     public void turn(Map<Point, List<Point>> laby, int labyWidth, int labyHeight, Point position, List<Point> fromages) {
-        Point pt1 = new Point(2,1);
-        Point pt2 = new Point(3,1);
+        Point pt1 = new Point(2, 1);
+        Point pt2 = new Point(3, 1);
         System.out.println((fromageIci(pt1) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt1);
         System.out.println((fromageIci_EnOrdreConstant(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
         System.out.println((passagePossible(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
@@ -26,31 +40,49 @@ public class PyRat {
     /* Regarde dans la liste des fromages s’il y a un fromage à la position pos.
         @return true s'il y a un fromage à la position pos, false sinon. */
     private boolean fromageIci(Point pos) {
-        return false;
+
+        return fromageList.contains(pos);
     }
 
     /* Regarde de manière performante (accès en ordre constant) s’il y a un fromage à la position pos.
         @return true s'il y a un fromage à la position pos, false sinon. */
     private boolean fromageIci_EnOrdreConstant(Point pos) {
-        return false;
+
+        return fromageSet.contains(pos);
     }
 
     /* Indique si le joueur peut passer de la position (du Point) « de » au point « a ».
         @return true s'il y a un passage depuis  « de » vers « a ». */
     private boolean passagePossible(Point de, Point a) {
-        return false;
+
+        return laby.get(de) != null && laby.get(de).contains(a);
     }
 
     /* Indique si le joueur peut passer de la position (du Point) « de » au point « a »,
         mais sans devoir parcourir la liste des Points se trouvant dans la Map !
         @return true s'il y a un passage depuis  « de » vers « a ». */
     private boolean passagePossible_EnOrdreConstant(Point de, Point a) {
-        return false;
+        return passageOrdreConstant.get(de).contains(a);
     }
 
     /* Retourne la liste des points qui ne peuvent pas être atteints depuis la position « pos ».
         @return la liste des points qui ne peuvent pas être atteints depuis la position « pos ». */
     private List<Point> pointsInatteignables(Point pos) {
-        return null;
+        Set<Point>  labyPoints= new HashSet<>(laby.keySet());
+        labyPoints.removeAll(this.backTrack(pos, new HashSet<>()));
+        return new ArrayList<>(labyPoints);
+
+    }
+
+
+    private Set<Point> backTrack(Point pos, Set<Point> atteignable) {
+        atteignable.add(pos);
+        for (Point point : passageOrdreConstant.get(pos)) {
+            if(!atteignable.contains(point)){
+                backTrack(point, atteignable);
+            }
+        }
+        return atteignable;
     }
 }
+
